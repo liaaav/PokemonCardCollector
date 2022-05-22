36befdd620e42ec3527dd3240e533c8545848b1f
liaaav <90356115+liaaav@users.noreply.github.com>
2022-05-23 11:34:16 +1200

    Update Collection.java

--- Collection.java
+++ Collection.java
@@ -1,17 +1,17 @@
 import java.util.HashMap;
 import ecs100.*;
 /**
- * Write a description of class Collection here.
+ * Collection of pokemon cards
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author AL
+ * @version 20/05/22
  */
 public class Collection
 {
     // instance variables - replace the example below with your own
     private HashMap<Integer, pokemonCard> Collection;     //declaring the hashmap
-    private int currpokemonCardId;                     // store the current if of pokemonCard being added
-    private pokemonCard currpokemonCard;                      // store the instance of the current pokemonCard
+    private int currPokemonCardId;                     // store the current if of pokemonCard being added
+    private pokemonCard currPokemonCard;                      // store the instance of the current pokemonCard
     /**
      * Constructor for objects of class Collection
      */
@@ -21,36 +21,37 @@ public class Collection
         Collection = new HashMap<Integer, pokemonCard>();
         
         // create Collection
-        pokemonCard c1 = new pokemonCard("Lucy Harris", "027882282", "lucyHarris.jpg");
-        pokemonCard c2 = new pokemonCard("Bella Domaneschi", "020666666");
-        pokemonCard c3 = new pokemonCard("Lucy Domaneschi", "020666666");
+        pokemonCard p1 = new pokemonCard("wooper", 0.34, "wooper.png");
+        pokemonCard p2 = new pokemonCard("wooper", 7.45);
+        pokemonCard p3 = new pokemonCard("galarian ponyta", 7.76);
         
-        Collection.put(1, c1);
-        Collection.put(2, c2);
-        Collection.put(3, c3);
+        Collection.put(1, p1);
+        Collection.put(2, p2);
+        Collection.put(3, p3);
         
-        this.currpokemonCardId = 3;     //Stores the current pokemonCard id
+        this.currPokemonCardId = 3;     //Stores the current pokemonCard id
     }
 
     /**
-     * Set pokemonCardId
+     * Set pokemon Card Id
      */
-    public void setpokemonCardId(){
-        this.currpokemonCardId += 1;
+    public void setPokemonCardId(){
+        this.currPokemonCardId += 1;
     }
     
     /**
-     * Add a pokemonCard to the map
+     * Add a pokemon Card to the map
      */
-    public void addpokemonCard(String name, String number, String img) {
-        Collection.put(currpokemonCardId, new pokemonCard(name, number, img));
+    public void addPokemonCard(String name, double val, String img) {
+        
+        Collection.put(currPokemonCardId, new pokemonCard(name, val, img));
     }
     
     /** 
      * Getter for the current pokemonCard instance
      */
-    public pokemonCard getpokemonCard(){
-        return this.currpokemonCard;
+    public pokemonCard getPokemonCard(){
+        return this.currPokemonCard;
     }
     
     /**
@@ -58,11 +59,11 @@ public class Collection
      * Sets the current pokemonCard instance if found
      * @return boolean false if not found
      */
-    public boolean findpokemonCard(String name){
+    public boolean findPokemonCard(String name){
         // Search for pokemonCard
         for (int pokemonCardId: Collection.keySet()){
             if (Collection.get(pokemonCardId).getName().toLowerCase().contains(name)){
-                currpokemonCard = Collection.get(pokemonCardId);
+                currPokemonCard = Collection.get(pokemonCardId);
                 return true;
             }
         }
@@ -77,7 +78,7 @@ public class Collection
         UI.clearText();
         for (int pokemonCardId : Collection.keySet()){
             UI.println(pokemonCardId+") " + Collection.get(pokemonCardId).getName());
-            UI.println("Number: " + Collection.get(pokemonCardId).getNumber());
+            UI.println("Value: $ " + Collection.get(pokemonCardId).getValue());
         }
     }
     
