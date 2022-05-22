91e4eb75d91c93db3ee6b86ec66acce3a11afc16
liaaav <90356115+liaaav@users.noreply.github.com>
2022-05-23 11:34:31 +1200

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,6 +1,6 @@
 import ecs100.*;
 /**
- * Write a description of class GUI here.
+ * code of GUI
  *
  * @author (your name)
  * @version (a version number or a date)
@@ -11,6 +11,7 @@ public class GUI
     private Collection collection;        // declare Collection instance
     private pokemonCard pokemonCard;          // declare pokemonCard sinstance
 
+
     /**
      * Constructor for objects of class GUI
      */
@@ -20,29 +21,26 @@ public class GUI
         collection = new Collection();       //instantiate the books object
         UI.initialise();
         UI.addButton("Print All", collection::printAll);
-        UI.addButton("Add", this::addpokemonCard);
-        UI.addButton("Find", this::findpokemonCard);
+        UI.addButton("Add", this::addPokemonCard);
+        UI.addButton("Find", this::findPokemonCard);
         UI.addButton("Hide Details", this::clear);
         UI.addButton("Quit", UI::quit);
         UI.setMouseListener(this::mouse);
     }
     
     /**
-     * Add a book to collection
+     * Add a pokemon card to collection
      */
-    public void addpokemonCard(){
+    public void addPokemonCard(){
         final int INCREMENT = 1;
         UI.clearText();
-        String number;
-        String name = UI.askString("Name: ");
-        do {
-            number = UI.askString("Number: ");
-        }while(isNumeric(number) != true);
+        String name = UI.askString("Name: ").toLowerCase();
+        double value = UI.askDouble("Value: $");
         
         // add a book image for display in the GUI
         String imgFileName = UIFileChooser.open("Choose Image File: ");
-        collection.setpokemonCardId(); // Increment the ID by one
-        collection.addpokemonCard(name, number, imgFileName); 
+        collection.setPokemonCardId(); // Increment the ID by one
+        collection.addPokemonCard(name, value, imgFileName); 
     }
     
     /**
@@ -61,18 +59,18 @@ public class GUI
      * Finds book based on name
      * Prints out the author and qty if found
      */
-    public void findpokemonCard(){
-        String pokemonCardName = UI.askString("Enter name of pokemonCard");
-        if (collection.findpokemonCard(pokemonCardName.toLowerCase())){
+    public void findPokemonCard(){
+        String pokemonCardName = UI.askString("Enter name of Pokemon");
+        if (collection.findPokemonCard(pokemonCardName.toLowerCase())){
             UI.clearText();
             UI.clearGraphics();
-            UI.println("Found pokemonCard!");
-            pokemonCard = collection.getpokemonCard();
+            UI.println("Found Pokemon!");
+            pokemonCard = collection.getPokemonCard();
             UI.println("Name: " + pokemonCard.getName());
-            UI.println("Number: " + pokemonCard.getNumber());
+            UI.println("Value: $" + pokemonCard.getValue());
             pokemonCard.displayProfile();  //Show profile on canvas
         }else{
-            UI.println("pokemonCard not found");
+            UI.println("Card not found :C");
         };
     }
     
